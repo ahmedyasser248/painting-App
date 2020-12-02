88835e4898968e877b219d2288d979a013be14e5
Ahmed Gamal <58369917+AG-Sign@users.noreply.github.com>
2020-12-02 16:50:43 +0200

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,19 @@
+<template>
+ <paintp />
+</template>
+
+<script>
+import paintp from './components/paint.vue'
+export default {
+  name: 'App',
+  components: {
+      paintp
+  }
+}
+</script>
+
+<style>
+#app {
+
+}
+</style>
